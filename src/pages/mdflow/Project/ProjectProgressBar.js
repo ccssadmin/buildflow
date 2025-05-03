2bfafa05da10d88205ade3a174c130208b3bcde0
ccshub001 <subash.selvaraj@crestclimbers.com>
2025-05-03 17:50:21 +0530

    Kanban All other flow

--- src/pages/mdflow/Project/ProjectProgressBar.js
+++ src/pages/mdflow/Project/ProjectProgressBar.js
@@ -1,19 +1,26 @@
 import React from "react";
 
 const ProjectProgressBar = ({ progress }) => {
-  // Normalize string values to 0
+  // Normalize string values to 0 or 5
   const normalizedProgress =
-  progress === "Pending" || 
-  progress === "Planned" || 
-  progress === "NotApproved" || 
-  progress === "OnHold" ? 0 : progress === "Approved"  ? 5 : progress === "InProgress" ? 50 : progress;
+    progress === "Pending" || 
+    progress === "Planned" || 
+    progress === "NotApproved" || 
+    progress === "Approved" || 
+    progress === "OnHold"
+      ? 0
+      : progress === "InProgress"
+      ? 20
+      : progress;
 
+  // Ensure left is never less than 3%
+  const leftPosition = Math.max(normalizedProgress, 3);
 
   return (
     <div className="progress-container mt-3 mb-2">
       <div className="custom-progress-bar">
         <div className="progress-fill" style={{ width: `${normalizedProgress}%` }} />
-        <div className="progress-indicator large" style={{ left: `${normalizedProgress}%` }}>
+        <div className="progress-indicator large" style={{ left: `${leftPosition}%` }}>
           {normalizedProgress}%
         </div>
       </div>
